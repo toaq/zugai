8eacf8db430141037ed869dbca5735d40b14815f
Lynn <lynn@foldr.moe>
2022-03-19 00:33:47 +0100

    what did I get myself into

--- interpret.pl
+++ interpret.pl
@@ -2,15 +2,44 @@
 :- [dict].
 
 % mai4 sa poq4 sa poq4 da
-% il(da,∃(poq1,∧(poq(poq1),∃(poq2,∧(poq(poq2),mai(poq1,poq2))))))
+% il(da,∃(p,∧(poq(p),∃(q,∧(poq(q),mai(p,q))))))
 
+% scope:
+% scope{ quant: [∃-p, ∃-q]
+%      , vars: vars{poq: q, aq: p, ho: q} }
+%
 
-interpret(discourse(D), Ls) :-
-  maplist(interpret, D, Lss),
-  append(Lss, Ls).
+push_scope :-
+  b_getval(scopes, S),
+  b_setval(scopes, [scope{quant:[], vars:[]} | S]).
 
-interpret(sentence(sentence_connector(C),Statement,illocution(I)), Ls) :-
-  interpret(Statement, [L1]),
-  (I = [] -> L2=L1 ; L2=il(I,L1)),
-  (C = [] -> L3=L2 ; L3=sc(C,L2)),
-  Ls = [L3].
+pop_scope(Out) :-
+  b_getval(scopes, [Out|S]),
+  b_setval(scopes, S).
+
+interpret(discourse(D), Formulas) :-
+  b_setval(scopes, []),
+  maplist(interpret, D, Fss),
+  append(Fss, Formulas).
+
+interpret(sentence(sentence_connector(C),Statement,illocution(I)), Formulas) :-
+  interpret(Statement, [F1]),
+  (I = [] -> F2=F1 ; F2=il(I,F1)),
+  (C = [] -> F3=F2 ; F3=sc(C,F2)),
+  Formulas = [F3].
+
+interpret(statement(Prenex, Pred), Formulas) :-
+  push_scope,
+  interpret(Prenex, _Topics),
+  interpret(Pred, F),
+  pop_scope(scope{quant: _Qs, vars: _V}),
+  % todo: apply quants
+  Formulas = [F].
+
+interpret(prenex(_Ts), Topics) :-
+  % todo
+  Topics = [].
+
+interpret(predication(_P, _Ts), _Formula) :-
+  % todo
+  !.
