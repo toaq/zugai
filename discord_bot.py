9e30e53b2b87a2fb2c2364ab071dfd333a3fe359
Lynn <lynn@foldr.moe>
2022-07-29 13:41:09 +0200

    Refactor Discord bot, add timeout

--- discord_bot.py
+++ discord_bot.py
@@ -4,6 +4,21 @@ import subprocess
 
 client = discord.Client()
 
+class RunException(Exception):
+    pass
+
+def run(verbing, cmd_args, **kwargs):
+    try:
+        result = subprocess.run(cmd_args, **kwargs, check=True, timeout=7, capture_output=True)
+        return result.stdout
+    except subprocess.CalledProcessError as e:
+        if e.stderr:
+            raise RunException(f"Error while {verbing}:\n```\n{e.stderr.decode().strip()}\n```")
+        else:
+            raise RunException(f"Error while {verbing}.")
+    except subprocess.TimeoutExpired as e:
+        raise RunException(f"Timed out while {verbing}.")
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
@@ -17,62 +32,37 @@ async def on_message(message):
     cmd, sentence = message.content.split(None, 1)
     cmd = cmd.strip('%')
 
-    if cmd == "tree":
-        # run "stack install" to put zugai-exe on path
-        run1 = subprocess.run(["zugai-exe", "--to-xbar-svg"], input=sentence.encode(), capture_output=True)
-        if run1.returncode != 0:
-            print(run1)
-            await message.channel.send(f'Failed to parse.\n```\n{run1.stderr.decode().strip()}\n```')
-            return
-
-        # put "inkscape" on path
-        run2 = subprocess.run(["inkscape", "--pipe", "-h", "800", "--export-filename=-", "--export-type=png"], input=run1.stdout, capture_output=True)
-        if run2.returncode != 0:
-            print(run2)
-            await message.channel.send('Failed to convert to png.')
-            return
-
-        file = discord.File(io.BytesIO(run2.stdout), filename="image.png")
-        #embed = discord.Embed()
-        #embed.set_thumbnail(url="attachment://image.png")
-        await message.channel.send(file=file)
-    elif cmd == "forest":
-        run1 = subprocess.run(["zugai-exe", "--to-xbar-latex"], input=sentence.encode(), capture_output=True)
-        if run1.returncode != 0:
-            print(run1)
-            await message.channel.send(f'Failed to parse.\n```\n{run1.stderr.decode().strip()}\n```')
-            return
-
-        with open("a.tex", "wb") as f: f.write(run1.stdout)
-
-        run2 = subprocess.run(["xelatex", "a.tex"])
-        if run2.returncode != 0:
-            print(run2)
-            await message.channel.send('Failed to convert to pdf.')
-            return
-
-        run3 = subprocess.run(["convert", "-density", "250", "-quality", "100", "-trim",
-            "-bordercolor", "#36393E", "-border", "30x30", "-define", "png:color-type=6",
-            "-background", "#36393E", "-alpha", "remove", "-alpha", "off",
-            "a.pdf", "a.png"])
-        if run3.returncode != 0:
-            print(run3)
-            await message.channel.send('Failed to convert to png.')
-            return
-
-        file = discord.File(open("a.png", "rb"), filename="image.png")
-        await message.channel.send(file=file)
-
-
-
-    elif cmd in ("english", "logic"):
-        run1 = subprocess.run(["zugai-exe", "--to-" + cmd], input=sentence.encode(), capture_output=True)
-        if run1.returncode != 0:
-            print(run1)
-            await message.channel.send(f'Failed to parse.\n```\n{run1.stderr.decode().strip()}\n```')
-            return
-        print(run1)
-        await message.channel.send(run1.stdout.decode())
+    try:
+        if cmd == "svg":
+            # run "stack install" to put zugai-exe on path
+            svg = run("parsing", ["zugai-exe", "--to-xbar-svg"], input=sentence.encode())
+            # put "inkscape" on path
+            png = run("converting to png", ["inkscape",
+                "--pipe", "-h", "800",
+                "--export-filename=-", "--export-type=png"], input=svg)
+            file = discord.File(io.BytesIO(png), filename="image.png")
+            await message.channel.send(file=file)
+        elif cmd == "tree":
+            tex = run("parsing", ["zugai-exe", "--to-xbar-latex"], input=sentence.encode())
+            with open("a.tex", "wb") as f: f.write(tex)
+            run("converting to pdf", ["xelatex", "a.tex"])
+            run("converting to png", ["convert",
+                "-define", "png:color-type=6",
+                "-density", "500", "-quality", "100",
+                "-background", "#36393E",
+                "-alpha", "remove", "-alpha", "off",
+                "-trim",
+                "-resize", "x1000>",
+                "-bordercolor", "#36393E", "-border", "40x20",
+                "a.pdf", "a.png"])
+            with open("a.png", "rb") as f:
+                file = discord.File(f, filename="image.png")
+                await message.channel.send(file=file)
+        elif cmd in ("english", "logic"):
+            txt = subprocess.run("parsing", ["zugai-exe", "--to-" + cmd], input=sentence.encode())
+            await message.channel.send(txt.decode())
+    except RunException as e:
+        await message.channel.send(str(e))
 
 client.run(os.environ['ZUGAI_DISCORD_TOKEN'])
 
