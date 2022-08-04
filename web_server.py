b01790b504723073d28a0a1146f314c848f95613
Lynn <lynn@foldr.moe>
2022-08-04 17:55:00 +0200

    Set CORS header in web_server

--- web_server.py
+++ web_server.py
@@ -2,26 +2,31 @@ from flask import Flask, request, Response
 import subprocess
 
 app = Flask(__name__)
-formats = ("english", "logic", "xbar-latex", "xbar-html", "xbar-json", "xbar-svg")
+formats = ("boxes", "english", "logic", "xbar-latex", "xbar-html", "xbar-json", "xbar-svg")
 usage = "Usage:\n" + "".join(f"GET /zugai?text=jadi&to={f}\n" for f in formats)
 
 mimetypes = {
+   'boxes':      'text/html',
    'english':    'text/plain',
    'logic':      'text/plain',
    'xbar-latex': 'application/x-tex',
    'xbar-html':  'text/html',
    'xbar-json':  'application/json',
-   'xbar-svg':   'image/svg+xml', 
+   'xbar-svg':   'image/svg+xml',
 }
+
+def cors(response):
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    return response
 
 @app.route('/zugai', methods=['GET'])
 def zugai():
     args = request.args
     to = args.get('to')
     if to not in formats:
-        return Response(usage, status=400, mimetype="text/plain")
+        return cors(Response(usage, status=400, mimetype="text/plain"))
     text = args.get('text', default='')
     run = subprocess.run(["zugai-exe", f"--to-{to}"], input=text.encode(), capture_output=True)
     if run.returncode != 0:
-        return Response(run.stderr, status=500, mimetype="text/plain")
-    return Response(run.stdout, mimetype=mimetypes[to])
+        return cors(Response(run.stderr, status=500, mimetype="text/plain"))
+    return cors(Response(run.stdout, mimetype=mimetypes[to]))
