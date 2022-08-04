3179109bb88b7412886ed438f46cef0b43db4867
Lynn <lynn@foldr.moe>
2022-08-04 18:03:45 +0200

    Add 'structure' to web_server formats

--- web_server.py
+++ web_server.py
@@ -2,18 +2,17 @@ from flask import Flask, request, Response
 import subprocess
 
 app = Flask(__name__)
-formats = ("boxes", "english", "logic", "xbar-latex", "xbar-html", "xbar-json", "xbar-svg")
-usage = "Usage:\n" + "".join(f"GET /zugai?text=jadi&to={f}\n" for f in formats)
-
-mimetypes = {
+formats = {
    'boxes':      'text/html',
    'english':    'text/plain',
    'logic':      'text/plain',
+   'structure':  'text/plain',
    'xbar-latex': 'application/x-tex',
    'xbar-html':  'text/html',
    'xbar-json':  'application/json',
    'xbar-svg':   'image/svg+xml',
 }
+usage = "Usage:\n" + "".join(f"GET /zugai?text=jadi&to={f}\n" for f in sorted(formats))
 
 def cors(response):
     response.headers["Access-Control-Allow-Origin"] = "*"
@@ -29,4 +28,4 @@ def zugai():
     run = subprocess.run(["zugai-exe", f"--to-{to}"], input=text.encode(), capture_output=True)
     if run.returncode != 0:
         return cors(Response(run.stderr, status=500, mimetype="text/plain"))
-    return cors(Response(run.stdout, mimetype=mimetypes[to]))
+    return cors(Response(run.stdout, mimetype=formats[to]))
