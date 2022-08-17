afe056b81877ad67fc51831b2633dbe9fc28fd1b
Lynn <lynn@foldr.moe>
2022-08-17 21:18:30 +0200

    Disallow "lu la" in a less clunky way

--- src/Xbar.hs
+++ src/Xbar.hs
@@ -472,10 +472,10 @@ instance ToXbar VpN where
   toXbar (Vlu lu stmt ky) = do
     xV <- mkTag "V" =<< toXbar lu
     xCP <- toXbar stmt
-    case xCP of
-      Pair _ _ xC _ -> do
-        c <- T.takeWhile (/= '[') . T.strip <$> aggregateSrc xC
-        when (c /= "") $ error $ "lu cannot be followed by an overt complementizer (\"lu " <> T.unpack c <> "\" is invalid)"
+    case stmt of
+      Statement (Just c) _ _ ->
+        error $ "lu cannot be followed by an overt complementizer (\"lu " <> T.unpack (toSrc c) <> "\" is invalid)"
+      _ -> pure ()
     xVP <- mkPair "VP" xV xCP
     terminated "V" xVP ky
   toXbar (Vverb w) = do
